a96c476963660000a59657ec6f1e2686e2fb2913
sheppardjm <jsheppard@neucadia.com>
2024-10-09 18:20:28 -0400

    Implement basic router

---
+++ services/Router.js
@@ -0,0 +1,41 @@
+const Router = {
+  init: () => {
+    document.querySelectorAll("a.navlink").forEach((a) => {
+      a.addEventListener("click", (e) => {
+        e.preventDefault();
+        const url = e.target.getAttribute("href");
+        Router.go(url);
+      });
+    });
+    Router.go(location.pathname);
+  },
+  go: (route, addToHistory = true) => {
+    console.log(`Going to ${route}`);
+
+    if (addToHistory) {
+      history.pushState({ route }, null, route);
+    }
+    let pageElement = null;
+    switch (route) {
+      case "/":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Menu";
+        break;
+      case "/order":
+        pageElement = document.createElement("h1");
+        pageElement.textContent = "Order";
+        break;
+    }
+    if (pageElement === null) {
+      pageElement = document.createElement("h1");
+      pageElement.textContent = "404 Not Found";
+    } else {
+      const cache = document.querySelector("main");
+      cache.innerHTML = "";
+      cache.appendChild(pageElement);
+      window.scrollX = 0;
+      window.scrollY = 0;
+    }
+  },
+};
+export default Router;
